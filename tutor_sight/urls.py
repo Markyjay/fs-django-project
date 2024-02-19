6f6771eab10c6cd7b87bada4b465fad4f4e83787
Mark Young <Markyjay@gmail.com>
2024-02-19 18:04:52 +0000

    align image and content on index.html

--- tutor_sight/urls.py
+++ tutor_sight/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.Index.as_view(), name='Index'),
-    path('teacher/<int:teacher_id>/', views.TeacherProfileDetail.as_view, name='teacherprofiledetail'),
+    path('teacher/<int:teacher_id>/', views.TeacherProfileDetail.as_view(), name='teacherprofiledetail'),
     path('booking/<int:teacher_id>', views.create_booking, name='create_booking'),
     path('booking/<int:pk>/delete/', views.delete_booking, name='delete_booking'),
 ]
